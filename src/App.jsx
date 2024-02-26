943d433561d1fcb5506cd7d84c92c947a87cd10b
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-02-26 05:00:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { MdDelete } from "react-icons/md";
 import { FaCheck } from "react-icons/fa6";
